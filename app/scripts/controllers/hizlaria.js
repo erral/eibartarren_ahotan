e92b52af41e5f5cf12e004967cba8cbe570a5edd
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-10-25 12:43:20 +0200

    bideoak txertatu zuzenean hizlariaren bistan

--- app/scripts/controllers/hizlaria.js
+++ app/scripts/controllers/hizlaria.js
@@ -8,15 +8,20 @@
  * Controller of the ahotanApp
  */
 angular.module('ahotanApp')
-  .controller('HizlariaCtrl', function ($scope, $routeParams, hizlariaService) {
+  .controller('HizlariaCtrl', function ($scope, $routeParams, hizlariaService, $sce) {
     hizlariaService.getHizlaria($routeParams.hizlaria)
         .then(function(hizlaria) {
-            console.log(hizlaria.data);
             $scope.hizlaria = hizlaria.data;
         });
     hizlariaService.getHizlariarenPasarteak($routeParams.hizlaria)
         .then(function(pasarteak) {
-            console.log(pasarteak.data);
-            $scope.pasarteak = pasarteak.data;
+            var pasarte_guztiak = [];
+            for (var i=0; i < pasarteak.data.length; i++){
+                var pasa = pasarteak.data[i];
+                pasa['audio_player'] = $sce.trustAsHtml(pasa['audioa_html']);
+                pasa['bideo_player'] = $sce.trustAsHtml(pasa['bideoa_html']);
+                pasarte_guztiak.push(pasa);
+            }
+            $scope.pasarteak = pasarte_guztiak;
         });
   });
